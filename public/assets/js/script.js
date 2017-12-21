c6a4b3c98b520904dedd3b1ca75c3ad3d07f4d0d
Reid Crawford <reidcrawford55@gmail.com>
2017-12-20 16:28:03 -0800

    added more functionality for hamburger button in responsive header

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,6 +7,11 @@ $(function(){
 	$(".navbar-toggler").on("click", function(e){
 		$(".fa-bars").toggleClass("hamburger-blue");
 	});
+	
+	$("#responsive-header li a").on("click", function(e){
+		console.log(e);
+		$(".navbar-toggler").click();
+	});
 });
 
 $.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten', function (data) {
