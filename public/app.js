a53e04b05c8a56442e8c6d695fe2de940454d341
Reid Crawford <reidcrawford55@gmail.com>
2017-12-22 17:48:56 -0800

    made projects an array of objects and displaying the on the front end

--- public/app.js
+++ public/app.js
@@ -1,5 +1,24 @@
-// (function(){
+(function(){
   angular.module('app', ['ui.router'])
+    .controller('mainCtrl', function($scope){
+       $scope.projects = [
+       {
+        name: "BuzzBar", 
+        href: "https://buzzbaricecream.com/",
+        description: "Wordpress Website"
+       },
+       {
+        name: "TravelZeeky",
+        href: "https://travelzeeky.com/allroads",
+        description: "Travel site for dicounts made with PHP"
+       }
+    ]
+    var projects = $scope.projects;
+       console.log(projects);
+       for (var i = 0; i < projects.length; i++) {
+            console.log(projects[i].name);
+        }
+    })
     .config(function($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/home');
@@ -9,12 +28,12 @@
         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('home', {
             url: '/home',
-            templateUrl: '/views/home.html'       
+            templateUrl: '/views/home.html',      
+            controller: 'mainCtrl'
         })
         .state('about', {
             url: '/about',
             templateUrl: '/views/about.html'       
         });
-
-});
-
+    });
+})()
