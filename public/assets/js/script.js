28d4be7c7de81e4b89c9d8778a33942b99febdea
Reid Crawford <reidcrawford55@gmail.com>
2017-12-25 14:03:13 -0800

    updated logic for JS file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,8 @@
-$.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten', function (data) {
-  console.log(data);
-  for(var i = 0; i < data.length; i++) {
-     	console.log(data[i].punchline);
-   }
-})
+$(function(){
+	$.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten', function (data) {
+	  console.log(data);
+	  for(var i = 0; i < data.length; i++) {
+	     	console.log(data[i].punchline);
+	   }
+	})
+});
