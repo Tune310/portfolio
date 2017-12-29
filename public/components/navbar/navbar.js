2bf9f11f823f92c65ad08bb8dd46e5dd6d0b5c1c
Reid Crawford <reidcrawford55@gmail.com>
2017-12-29 14:19:51 -0800

    updated navbar component logic

--- public/components/navbar/navbar.js
+++ public/components/navbar/navbar.js
@@ -1,5 +1,9 @@
 angular
 .module('app')
+.controller("MainNavController", function($scope){
+	// Code For MainNavController Here
+})
 .component('navbar', {
-    templateUrl: "components/navbar/navbar.html"
+    templateUrl: "components/navbar/navbar.html",
+    controller: "MainNavController"
 })
